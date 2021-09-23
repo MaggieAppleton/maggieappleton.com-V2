6fe9738809c72be53c8a916dfcd95994655f7903
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-23 11:54:47 +0100

    setup next dynamic routing

--- utils/mdxUtils.js
+++ utils/mdxUtils.js
@@ -1,11 +1,20 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs";
+import path from "path";
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), 'posts')
+export const POSTS_PATH = path.join(process.cwd(), "posts");
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePaths = fs
-  .readdirSync(POSTS_PATH)
-  // Only include md(x) files
-  .filter((path) => /\.mdx?$/.test(path))
+    .readdirSync(POSTS_PATH)
+    // Only include md(x) files
+    .filter((path) => /\.mdx?$/.test(path));
+
+// POSTS_PATH is useful when you want to get the path to a specific file
+export const NOTES_PATH = path.join(process.cwd(), "posts", "notes");
+
+// postFilePaths is the list of all mdx files inside the POSTS_PATH directory
+export const noteFilePaths = fs
+    .readdirSync(NOTES_PATH)
+    // Only include md(x) files
+    .filter((path) => /\.mdx?$/.test(path));
