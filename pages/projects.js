d43ace1b2cf0f8c44be00f3088d10e64b7f54424
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-09-25 10:14:51 +0100

    metadata cleanup

---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Projects() {
+    return (
+        <Layout>
+            <h1>Projects</h1>
+            <p>Stuff</p>
+        </Layout>
+    );
+}
