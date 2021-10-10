31d7368c97422d6840a6650b7e3fc1d0c873bc2a
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-10-10 11:50:25 +0100

    fixed illustration page

---
+++ pages/illustration.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+import { Title1, Title2 } from "../components/Typography";
+import Header from "../components/Header";
+
+export default function Illustration() {
+    return (
+        <>
+            <Header title="Illustrations by Maggie Appleton" />
+            <Layout>
+                <Title1>Illustrations</Title1>
+                <Title2>Welcome</Title2>
+            </Layout>
+        </>
+    );
+}
