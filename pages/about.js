6f85ceb385e2d41bd94abe27256ca72b9189157d
Maggie Appleton <maggie.fm.appleton@gmail.com>
2021-10-31 17:53:11 +0000

    finished about page

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,14 @@
 import Layout from "../components/Layout";
 import { Title1, Title2, SmallCaps } from "../components/Typography";
+import Link from "next/link";
 import Image from "next/image";
 import Header from "../components/Header";
 import styled from "styled-components";
 import { breakpoints } from "../utils/breakpoints";
 import { Spacer } from "../components/Spacer";
 import UnderlineHoverLink from "../components/links/UnderlineHoverLink";
+import LeafIcon from "../components/icons/LeafIcon";
+import { RelativeDate } from "../components/templates/Dates";
 
 export default function About() {
     return (
@@ -133,15 +136,16 @@ export default function About() {
                             </p>
                         </div>
                         <Image
-                            src="/images/profile_bw_700.png"
+                            src="/images/profile_bw_800.png"
                             alt="a photo of maggie appleton"
-                            width={700 / 1.5}
-                            height={922 / 1.5}
+                            width={800 / 1.5}
+                            height={1141 / 1.5}
                             layout="responsive"
                             objectFit="contain"
                         />
                     </section>
-                    <Spacer />
+                    {/* <Spacer /> */}
+                    <Divider />
                     <section className="talks">
                         <Title2>Talks</Title2>
                         <p>
@@ -151,45 +155,59 @@ export default function About() {
                             anthropology topics and the narratives we tell
                             ourselves in the world of software.
                         </p>
-                        {[
-                            {
-                                title: "A Picture Worth a Thousand Programmes",
-                                slug: "/programming-pictures",
-                                conferences: ["React Advanced London"],
-                                description: "",
-                                date: "October 2021",
-                            },
-                            {
-                                title: "Tools for Thought as Cultural Systems, Not Computational Software",
-                                slug: "/programming-pictures",
-                                conferences: ["React Advanced London"],
-                                description: "",
-                                date: "October 2021",
-                            },
-                            {
-                                title: "The Cultural Anthropology of React",
-                                slug: "/programming-pictures",
-                                conferences: ["React Advanced London"],
-                                description: "",
-                                date: "October 2021",
-                            },
-                            {
-                                title: "Drawing the Invisible: React Explained in Five Visual Metaphors",
-                                slug: "/programming-pictures",
-                                conferences: [
-                                    "Women of React",
-                                    "egghead Talks",
-                                ],
-                                description: "",
-                                date: ["July 2020", "September 2020"],
-                            },
-                        ].map(({ title, conferences, date, slug }, i) => (
-                            <div key={i}>
-                                {title}
-                                {conferences}
-                                {date}
-                            </div>
-                        ))}
+                        <GridSection>
+                            {[
+                                {
+                                    title: "A Picture Worth a Thousand Programmes",
+                                    slug: "/programming-pictures",
+                                    conferences: "React Advanced London",
+
+                                    date: "October 2021",
+                                },
+                                {
+                                    title: "Tools for Thought as Cultural Systems, Not Computational Objects",
+                                    slug: "/tools-for-thought",
+                                    conferences: "Augmented Minds Unconference",
+
+                                    date: "July 2021",
+                                },
+                                {
+                                    title: "The Cultural Anthropology of React",
+                                    slug: "/anthropology-react",
+                                    conferences: "React Rally",
+
+                                    date: "August 2020",
+                                },
+                                {
+                                    title: "How to Become a Neo-Cartesian Cyborg",
+                                    slug: "neocyborgs",
+                                    conferences: "BASB London",
+                                    date: "May 2020",
+                                },
+                                {
+                                    title: "Evergreen Notes and Digital Gardens",
+                                    slug: "https://www.youtube.com/watch?v=RXXXHN516qc",
+                                    conferences: "Roam Research Tours",
+
+                                    date: "May 2020",
+                                },
+                                {
+                                    title: "Drawing the Invisible: React Explained in Five Visual Metaphors",
+                                    slug: "/reactpotato",
+                                    conferences: "Women of React",
+
+                                    date: "May 2020",
+                                },
+                            ].map(({ title, conferences, date, slug }, i) => (
+                                <TalkCard
+                                    key={i}
+                                    title={title}
+                                    host={conferences}
+                                    date={date}
+                                    url={slug}
+                                />
+                            ))}
+                        </GridSection>
                         <div className="images">
                             <Image
                                 src="/images/talks_1.jpg"
@@ -207,11 +225,107 @@ export default function About() {
                             />
                         </div>
                     </section>
+                    <Spacer />
+                    <section className="podcasts">
+                        <Title2>Podcasts</Title2>
+                        <p>
+                            A handful of kind and interesting people have been
+                            gracious enough to let me ramble about programming
+                            metaphors on their podcasts.
+                        </p>
+                        <GridSection>
+                            {[
+                                {
+                                    podcastName: "Frontend Heroes",
+                                    episodeName: "Fortress of Solitude",
+                                    date: "August 10, 2021",
+                                    url: "https://frontendheroes.transistor.fm/episodes/fortress-of-solitude-w-maggie-appleton",
+                                },
+                                {
+                                    podcastName: "Metamuse",
+                                    episodeName: "Visual Programming",
+                                    date: "August 5, 2021",
+                                    url: "https://museapp.com/podcast/37-visual-programming/",
+                                },
+                                {
+                                    podcastName: "The Swyx Mixtape",
+                                    episodeName:
+                                        "Growing Digital Gardens and Tending the Web",
+                                    date: "February 13 2021",
+                                    url: "https://podcasts.apple.com/us/podcast/weekend-drop-digital-gardening-w-maggie-appleton/id1549059398?i=1000508923478",
+                                },
+                                {
+                                    podcastName: "RoamFM",
+                                    episodeName:
+                                        "Digital Anthropology, Digital Gardens and Illustrated Notes",
+                                    date: "July 25 2020",
+                                    url: "https://thatsthenorm.com/maggie-appleton-transcript/",
+                                },
+                                {
+                                    podcastName: "Maintainers Anonymous",
+                                    episodeName: "Embodiment Through Metaphors",
+                                    date: "May 13 2020",
+                                    url: "https://maintainersanonymous.com/metaphor/",
+                                },
+                                {
+                                    podcastName: "egghead.io Podcast",
+                                    episodeName:
+                                        "Turning Technical Concepts into Approachable Illustrated Metaphors",
+                                    date: "May 3, 2019",
+                                    url: "https://egghead.io/podcasts/turning-technical-concepts-into-approachable-illustrated-metaphors-with-maggie-appleton",
+                                },
+                                {
+                                    podcastName: "Maintainers Anonymous",
+                                    episodeName:
+                                        "Open Source as a Gift Economy",
+                                    date: "March 5 2020",
+                                    url: "https://maintainersanonymous.com/gift/",
+                                },
+                                {
+                                    podcastName: "React Podcast",
+                                    episodeName: "The Power of Mental Model‪s",
+                                    date: "March 3, 2020",
+                                    url: "https://reactpodcast.simplecast.com/episodes/86",
+                                },
+                            ].map(
+                                (
+                                    { podcastName, episodeName, date, url },
+                                    i
+                                ) => (
+                                    <TalkCard
+                                        key={i}
+                                        title={podcastName}
+                                        host={episodeName}
+                                        date={date}
+                                        url={url}
+                                    />
+                                )
+                            )}
+                        </GridSection>
+                    </section>
                 </StyledMain>
             </Layout>
         </>
     );
 }
+
+const Divider = styled.hr`
+    margin: var(--space-96) 0;
+    height: 2px;
+    background-color: var(--color-salmon);
+    border: none;
+    width: 20%;
+    @media ${breakpoints.mediaSM} {
+        margin: var(--space-64) 0;
+    }
+`;
+
+const GridSection = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, auto));
+    gap: var(--space-32);
+    margin: var(--space-48) 0;
+`;
 
 const HeaderSection = styled.header`
     margin: var(--space-24) auto 0;
@@ -232,18 +346,20 @@ const StyledMain = styled.main`
     padding: var(--space-64) 0;
     width: 100%;
     @media ${breakpoints.mediaSM} {
-        padding: var(--space-80) var(--space-16);
+        padding: var(--space-48) var(--space-16);
     }
     img {
         border-radius: 4px;
     }
     h2 {
-        margin-bottom: var(--space-16);
+        margin-bottom: var(--space-24);
     }
     p {
         line-height: var(--leading-loose);
-        margin-bottom: var(--space-24);
         max-width: 52ch;
+    }
+    p:not(:last-of-type) {
+        margin-bottom: var(--space-24);
     }
     section.intro {
         display: grid;
@@ -253,6 +369,20 @@ const StyledMain = styled.main`
         ${SmallCaps}:not(:first-of-type) {
             margin-top: var(--space-64);
             color: var(--color-gray-900);
+        }
+        ${SmallCaps}::before {
+            content: "";
+            display: inline-block;
+            width: 20px;
+            clear: both;
+            height: 20px;
+            margin-right: -1.5rem;
+            background-image: url("/images/leaf-icon.svg");
+            background-size: contain;
+            background-repeat: no-repeat;
+            position: relative;
+            top: 4px;
+            right: 2.25rem;
         }
         @media ${breakpoints.mediaSM} {
             grid-template-columns: 1fr;
@@ -266,6 +396,99 @@ const StyledMain = styled.main`
             display: grid;
             grid-template-columns: repeat(auto-fit, minmax(280px, auto));
             gap: var(--space-24);
+        }
+    }
+`;
+
+function TalkCard({ title, host, date, url, key }) {
+    return (
+        <a key={key} href={url}>
+            <StyledTalkCard>
+                <div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: "var(--space-16)",
+                        }}
+                    >
+                        {/* <LeafIcon width="20" height="20" /> */}
+                        <svg
+                            fill="var(--color-sea-blue"
+                            xmlns="http://www.w3.org/2000/svg"
+                            preserveAspectRatio="xMinYMin meet"
+                            viewBox="0 0 3 20"
+                            width="3"
+                            height="20"
+                        >
+                            <rect width="3" height="20" />
+                        </svg>
+                        <h3>{title}</h3>
+                    </div>
+                    <span className="host">{host}</span>
+                    <span className="date">
+                        <RelativeDate postDate={date} />
+                    </span>
+                </div>
+            </StyledTalkCard>
+        </a>
+    );
+}
+
+const StyledTalkCard = styled.div`
+    display: flex;
+    flex-direction: row;
+    border-radius: var(--border-radius-base);
+    border: 1px solid var(--color-cream);
+    transition: all 0.3s ease-in-out;
+    h3 {
+        color: var(--color-gray-800);
+        transition: all 0.3s ease-in-out;
+        font-family: var(--font-body);
+        font-size: var(--font-size-base);
+        font-weight: 400;
+        line-height: var(--leading-snug);
+        margin-bottom: var(--space-8);
+        transition: all 0.3s ease-in-out;
+    }
+    svg {
+        display: inline-block;
+        position: relative;
+        top: 6px;
+        flex-shrink: 0;
+        transition: all 0.3s ease-in-out;
+    }
+    div {
+        display: flex;
+        flex-direction: column;
+        span.host {
+            color: var(--color-gray-600);
+            font-family: var(--font-sans);
+            font-size: var(--font-size-sm);
+            margin: 0 0 var(--space-8) 1.3rem;
+            transition: all 0.3s ease-in-out;
+        }
+        span.date {
+            font-family: var(--font-sans);
+            font-size: calc(var(--font-size-xs) / 1.08);
+            color: var(--color-gray-600);
+            text-transform: uppercase;
+            letter-spacing: 0.05em;
+            font-weight: 400;
+            margin: 0 0 var(--space-8) 1.3rem;
+            transition: all 0.3s ease-in-out;
+        }
+    }
+    &:hover {
+        h3 {
+            color: var(--color-bright-crimson);
+            transform: translateX(6px);
+        }
+        span {
+            transform: translateX(6px);
+        }
+        svg {
+            transform: scaleX(2.5) translateX(1px);
         }
     }
 `;
