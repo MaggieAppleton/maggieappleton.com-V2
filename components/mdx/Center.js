19c1670ea77776757e4fa2b2188030e92ff5b4d0
Maggie Appleton <appleton.m@outlook.com>
2021-11-20 10:59:28 +0000

    fixed centre component

--- components/mdx/Center.js
+++ components/mdx/Center.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Center = styled.div`
     text-align: center;
     margin: inherit auto;
+    font-family: var(--font-body);
 `;
 
 export default Center;
