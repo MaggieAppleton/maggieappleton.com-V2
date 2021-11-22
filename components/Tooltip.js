61ac117bd9fa3a96f301fe963f44ab94471b1a0c
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2021-11-22 08:25:58 +0000

    fixed tooltip issue

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -5,32 +5,33 @@ import "tippy.js/dist/tippy.css";
 import "tippy.js/animations/shift-away.css";
 
 const Tooltip = forwardRef(({ content, children }, ref) => {
-    return (
-        <StyledTippy
-            duration="500"
-            arrow={true}
-            interactive={true}
-            animation="shift-away"
-            content={content}
-        >
-            <span ref={ref}>{children}</span>
-        </StyledTippy>
-    );
+  return (
+    <StyledTippy
+      duration="500"
+      arrow={true}
+      interactive={true}
+      animation="shift-away"
+      content={content}
+    >
+      <span ref={ref}>{children}</span>
+    </StyledTippy>
+  );
 });
 
 const StyledTippy = styled(Tippy)`
-    font-family: var(--font-sans);
-    font-size: var(--font-size-sm);
-    word-break: break-word;
-    white-space: pre-wrap;
-    padding: var(--space-3xs);
-    margin-bottom: var(--space-3xs);
-    background-color: white;
-    color: var(--color-gray-800);
-    box-shadow: var(--box-shadow-md);
-    .tippy-arrow {
-        color: white;
-    }
+  font-family: var(--font-sans);
+  font-size: var(--font-size-sm);
+  line-height: var(--leading-snug);
+  word-break: break-word;
+  white-space: pre-wrap;
+  padding: var(--space-3xs);
+  margin-bottom: var(--space-3xs);
+  background-color: white;
+  color: var(--color-gray-800);
+  box-shadow: var(--box-shadow-md);
+  .tippy-arrow {
+    color: white;
+  }
 `;
 
 export default Tooltip;
--- components/links/TooltipLink.js
+++ components/links/TooltipLink.js
@@ -3,144 +3,144 @@ import styled from "styled-components";
 import { ExternalLinkIcon } from "@heroicons/react/solid";
 
 export default function TooltipLink({ href, children }) {
-    if (href.startsWith("http")) {
-        return <ExternalLink href={href}>{children}</ExternalLink>;
-    }
-    return <InternalLink href={href}>{children}</InternalLink>;
+  if (href.startsWith("http")) {
+    return <ExternalLink href={href}>{children}</ExternalLink>;
+  }
+  return <InternalLink href={href}>{children}</InternalLink>;
 }
 
 function InternalLink({ href, children }) {
-    return (
-        <Tooltip
-            content={<StyledExternalUrl href={href}>{href}</StyledExternalUrl>}
-        >
-            <StyledContainer>
-                <StyledLink internal href={href}>
-                    <span>{children}</span>
-                </StyledLink>
-            </StyledContainer>
-        </Tooltip>
-    );
+  return (
+    <Tooltip
+      content={<StyledExternalUrl href={href}>{href}</StyledExternalUrl>}
+    >
+      <StyledContainer>
+        <StyledLink internal href={href}>
+          <span>{children}</span>
+        </StyledLink>
+      </StyledContainer>
+    </Tooltip>
+  );
 }
 
 function ExternalLink({ href, children }) {
-    return (
-        <Tooltip
-            content={
-                <StyledExternalUrl href={href}>
-                    {href}
-                    <ExternalLinkIcon
-                        width="18"
-                        height="18"
-                        style={{ position: "relative", top: "3px" }}
-                    />
-                </StyledExternalUrl>
-            }
-        >
-            <StyledContainer>
-                <StyledLink href={href}>
-                    <span>{children}</span>
-                </StyledLink>
-            </StyledContainer>
-        </Tooltip>
-    );
+  return (
+    <Tooltip
+      content={
+        <StyledExternalUrl href={href}>
+          {href}
+          <ExternalLinkIcon
+            width="18"
+            height="18"
+            style={{ position: "relative", top: "3px" }}
+          />
+        </StyledExternalUrl>
+      }
+    >
+      <StyledContainer>
+        <StyledLink href={href}>
+          <span>{children}</span>
+        </StyledLink>
+      </StyledContainer>
+    </Tooltip>
+  );
 }
 
 const StyledExternalUrl = styled.a`
-    color: var(--color-gray-600);
-    transition: color 0.2s ease-in-out;
-    text-align: center;
-    &:hover {
-        color: var(--color-bright-crimson);
-    }
+  color: var(--color-gray-600);
+  transition: color 0.2s ease-in-out;
+  text-align: center;
+  svg {
+    margin: 0 !important;
+  }
+  &:hover {
+    color: var(--color-bright-crimson);
+  }
 `;
 
 const StyledContainer = styled.div`
-    display: inline-block;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    left: 0px;
-    top: 0px;
+  display: inline-block;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  left: 0px;
+  top: 0px;
 `;
 
 const StyledLink = styled.a`
-    cursor: pointer;
-    position: relative;
-    white-space: nowrap;
-    outline: none;
+  cursor: pointer;
+  position: relative;
+  white-space: nowrap;
+  outline: none;
+  word-break: break-all;
+  &:focus {
+    outline: 2px solid darkblue;
+    border-radius: 2px;
+  }
+  &::before {
+    @media (min-width: 550px) {
+      content: "";
+      transform-origin: 50% 100%;
+      background: var(--color-gray-300);
+      transition: clip-path 0.3s, transform 0.3s cubic-bezier(0.2, 1, 0.8, 1);
+      position: absolute;
+      width: 100%;
+      height: 1px;
+      top: 105%;
+      left: 0;
+      pointer-events: none;
+      clip-path: polygon(
+        0% 0%,
+        0% 100%,
+        50% 100%,
+        50% 0,
+        50% 0,
+        50% 100%,
+        50% 100%,
+        0 100%,
+        100% 100%,
+        100% 0%
+      );
+    }
+  }
+  &:hover::before {
+    @media (min-width: 550px) {
+      transform: translate3d(0, 3px, 0) scale3d(1, 2, 1);
+      background: ${(props) =>
+        props.internal
+          ? "var(--color-bright-crimson)"
+          : "var(--color-sea-blue)"};
+      clip-path: polygon(
+        0% 0%,
+        0% 100%,
+        50% 100%,
+        50% 0,
+        50% 0,
+        50% 100%,
+        50% 100%,
+        0 100%,
+        100% 100%,
+        100% 0%
+      );
+    }
+  }
+  span {
+    display: inline-block;
+    white-space: normal;
+    transition: all 0.5s cubic-bezier(0.2, 1, 0.8, 1);
     word-break: break-all;
-    &:focus {
-        outline: 2px solid darkblue;
-        border-radius: 2px;
+    color: ${(props) =>
+      props.internal
+        ? "var(--color-medium-sea-blue)"
+        : "var(--color-bright-crimson)"};
+    text-decoration: underline;
+    @media (min-width: 550px) {
+      text-decoration: none;
     }
-    &::before {
-        @media (min-width: 550px) {
-            content: "";
-            transform-origin: 50% 100%;
-            background: var(--color-gray-300);
-            transition: clip-path 0.3s,
-                transform 0.3s cubic-bezier(0.2, 1, 0.8, 1);
-            position: absolute;
-            width: 100%;
-            height: 1px;
-            top: 105%;
-            left: 0;
-            pointer-events: none;
-            clip-path: polygon(
-                0% 0%,
-                0% 100%,
-                50% 100%,
-                50% 0,
-                50% 0,
-                50% 100%,
-                50% 100%,
-                0 100%,
-                100% 100%,
-                100% 0%
-            );
-        }
-    }
-    &:hover::before {
-        @media (min-width: 550px) {
-            transform: translate3d(0, 3px, 0) scale3d(1, 2, 1);
-            background: ${(props) =>
-                props.internal
-                    ? "var(--color-bright-crimson)"
-                    : "var(--color-sea-blue)"};
-            clip-path: polygon(
-                0% 0%,
-                0% 100%,
-                50% 100%,
-                50% 0,
-                50% 0,
-                50% 100%,
-                50% 100%,
-                0 100%,
-                100% 100%,
-                100% 0%
-            );
-        }
-    }
-    span {
-        display: inline-block;
-        white-space: normal;
-        transition: all 0.5s cubic-bezier(0.2, 1, 0.8, 1);
-        word-break: break-all;
-        color: ${(props) =>
-            props.internal
-                ? "var(--color-medium-sea-blue)"
-                : "var(--color-bright-crimson)"};
-        text-decoration: underline;
-        @media (min-width: 550px) {
-            text-decoration: none;
-        }
-    }
-    &:hover span {
-        transform: translate3d(0, -1px, 0);
-        color: ${(props) =>
-            props.internal
-                ? "var(--color-dark-sea-blue)"
-                : "var(--color-crimson)"};
-    }
+  }
+  &:hover span {
+    transform: translate3d(0, -1px, 0);
+    color: ${(props) =>
+      props.internal ? "var(--color-dark-sea-blue)" : "var(--color-crimson)"};
+  }
 `;
