2d106a671e73aa44d830d21577b43e00a9856a35
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2021-11-29 21:24:17 +0000

    added optimised book covers

--- posts/data/antibooks.js
+++ posts/data/antibooks.js
@@ -3,86 +3,75 @@ export const antiBookData = [
     title: "The Dawn of Everything",
     subtitle: "A New History of Humanity",
     author: "David Graeber",
-    cover:
-      "https://books.google.com/books?id=X8gZEAAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/dawn.jpeg",
     link: "https://www.google.com/books/edition/The_Dawn_of_Everything/X8gZEAAAQBAJ",
   },
   {
     title: "Turing's Cathedral",
     subtitle: "The Origins of the Digital Universe",
     author: "George Dyson",
-    cover:
-      "https://books.google.com/books?id=dMK3P6B3WgcC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/turing.jpeg",
     link: "https://www.google.com/books/edition/Turing's_Cathedral/dMK3P6B3WgcC",
   },
   {
     title: "New Dark Age",
     subtitle: "Technology, Knowledge and the End of the Future",
     author: "James Bridle",
-    cover:
-      "https://books.google.com/books?id=dD9gDwAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/newdarkage.jpeg",
     link: "https://www.google.com/books/edition/New_Dark_Age/dD9gDwAAQBAJ",
   },
   {
     title: "Staying with the Trouble",
     subtitle: "Making Kin in the Chthulucene",
     author: "Donna Haraway",
-    cover:
-      "https://pics.cdn.librarything.com/picsizes/f3/fd/f3fdfcb9cd5237459704b4478514345412f5945_v5.jpg",
+    cover: "/images/books/staying.jpeg",
     link: "https://www.google.com/books/edition/Staying_with_the_Trouble/cND9jwEACAAJ",
   },
   {
     title: "A People's History of Computing in the United States",
     author: "Joy Lisi Rankin",
-    cover:
-      "https://books.google.com/books?id=WnhyDwAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/peoplehistory.jpeg",
     link: "https://www.google.com/books/edition/A_People's_History_of_Computing_in_the_United_States/WnhyDwAAQBAJ",
   },
   {
     title: "A Small Matter of Programming",
     subtitle: "Perspectives on End User Computing",
     author: "Bonnie Nardi",
-    cover:
-      "https://books.google.com/books?id=0drDRT370eoC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/smallprog.jpeg",
     link: "https://www.google.com/books/edition/A_Small_Matter_of_Programming/0drDRT370eoC",
   },
   {
     title: "Supersizing the Mind",
     subtitle: "Embodiment, Action, and Cognitive Extension",
     author: "Andy Clark",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0199773688.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/supersize.jpg",
     link: "https://www.google.com/books/edition/Supersizing_the_Mind/I-tSEhdEorUC",
   },
   {
     title: "On Kings",
     author: "David Graeber & Marshall Sahlins",
-    cover:
-      "https://pics.cdn.librarything.com/picsizes/1d/4e/1d4ed1c54934223596e693778674345412f5945_v5.jpg",
+    cover: "/images/books/onkings.jpeg",
     link: "https://www.google.com/books/edition/On_Kings/Ef03jgEACAAJ",
   },
   {
     title: "Seeing Like a State",
     subtitle: "How Certain Schemes to Improve the Human Condition Have Failed",
     author: "James C. Scott",
-    cover:
-      "https://books.google.com/books?id=CA7UDwAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/seeingstate.jpeg",
     link: "https://www.google.com/books/edition/Seeing_Like_a_State/CA7UDwAAQBAJ",
   },
   {
     title: "Metahistory",
     subtitle: "The Historical Imagination in Nineteenth-Century Europe",
     author: "Hayden White",
-    cover:
-      "https://pics.cdn.librarything.com/picsizes/81/81/8181a86f89636be596a38496c774345412f5945_v5.jpg",
+    cover: "/images/books/metahistory.jpeg",
     link: "https://www.google.com/books/edition/Metahistory/v_DqhVZkQ2EC",
   },
   {
     title: "Hyperobjects",
     subtitle: "Philosophy and Ecology after the End of the World",
     author: "Timothy Morton",
-    cover:
-      "https://books.google.com/books?id=qu5zDwAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/hyperobjects.jpeg",
     link: "https://www.google.com/books/edition/Hyperobjects/qu5zDwAAQBAJ",
   },
 ];
--- posts/data/books.js
+++ posts/data/books.js
@@ -3,87 +3,76 @@ export const bookData = [
     title: "From Bacteria to Bach and Back",
     subtitle: "The Evolution of Minds",
     author: "Daniel C. Dennett",
-    cover:
-      "https://books.google.com/books?id=XuJoDQAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/bach.jpeg",
     link: "https://www.google.com/books/edition/From_Bacteria_to_Bach_and_Back/XuJoDQAAQBAJ",
   },
   {
     title: "Mind in Motion",
     subtitle: "How Action Shapes Thought",
     author: "Barbara Tversky",
-    cover:
-      "https://books.google.com/books?id=lv1wDwAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/mindmotion.jpg",
     link: "https://www.google.com/books/edition/Mind_in_Motion/lv1wDwAAQBAJ",
   },
   {
     title: "If Then",
     subtitle: "How One Data Company Invented the Future",
     author: "Jill Lepore",
-    cover:
-      "https://pics.cdn.librarything.com/picsizes/75/a9/75a9cd5f68e1018596f5a7277774345412f5945_v5.jpg",
+    cover: "/images/books/ifthen.jpg",
     link: "https://www.google.com/books/edition/If_Then/pODLDwAAQBAJ",
   },
   {
     title: "Work",
     subtitle: "A Deep History, from the Stone Age to the Age of Robots",
     author: "James Suzman",
-    cover:
-      "https://pics.cdn.librarything.com/picsizes/d7/16/d716db10cd30049596c636f77674345412f5945_v5.jpg",
+    cover: "/images/books/historywork.jpg",
     link: "https://www.google.com/books/edition/Work/e-UlEAAAQBAJ",
   },
   {
     title: "Sand Talk",
     subtitle: "How Indigenous Thinking Can Save the World",
     author: "Tyson Yunkaporta",
-    cover:
-      "https://books.google.com/books?id=-o6YDwAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/sandtalk.jpg",
     link: "https://www.google.com/books/edition/Sand_Talk/-o6YDwAAQBAJ",
   },
   {
     title: "Dealers of Lightning",
     subtitle: "Xerox PARC and the Dawn of the Computer Age",
     author: "Michael A. Hiltzik",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0887309895.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/dealers.jpeg",
     link: "https://www.google.com/books/edition/Dealers_of_Lightning/EylLmLzFCTMC",
   },
   {
     title: "Toward An Anthropological Theory of Value",
     subtitle: "The False Coin of Our Own Dreams",
     author: "David Graeber",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0312240457.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/theoryvalue.jpg",
     link: "https://www.google.com/books/edition/Toward_An_Anthropological_Theory_of_Value/nJPmwAEACAAJ",
   },
   {
     title: "Where the Action Is",
     subtitle: "The Foundations of Embodied Interaction",
     author: "Paul Dourish",
-    cover:
-      "https://books.google.com/books?id=DCIy2zxrCqcC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/wheretheactionis.jpg",
     link: "https://www.google.com/books/edition/Where_the_Action_is/DCIy2zxrCqcC",
   },
   {
     title: "Speculative Everything",
     subtitle: "Design, Fiction, and Social Dreaming",
     author: "Anthony Dunne",
-    cover:
-      "https://books.google.com/books?id=9gQyAgAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/speculative.png",
     link: "https://www.google.com/books/edition/Speculative_Everything/9gQyAgAAQBAJ",
   },
   {
     title: "The Timeless Way of Building",
     author: "Christopher Alexander",
-    cover:
-      "https://books.google.com/books?id=H6CE9hlbO8sC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/timeless.jpg",
     link: "https://www.google.com/books/edition/The_Timeless_Way_of_Building/H6CE9hlbO8sC",
   },
   {
     title: "Encoding Race, Encoding Class",
     subtitle: "Indian IT Workers in Berlin",
     author: "Sareeta Amrute",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0822361353.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/encoding.jpg",
     link: "https://www.google.com/books/edition/Encoding_Race,_Encoding_Class/RzO5DAAAQBAJ",
   },
   {
@@ -91,16 +80,14 @@ export const bookData = [
     subtitle:
       "How a Group of Hackers, Geniuses, and Geeks Created the Digital Revolution",
     author: "Walter Isaacson",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/147670869X.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/innovators.jpg",
     link: "https://www.google.com/books/edition/The_Innovators/aFapBAAAQBAJ",
   },
   {
     title: "Natural-born Cyborgs",
     subtitle: "Minds, Technologies, and the Future of Human Intelligence",
     author: "Andy Clark",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0195177517.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/naturalcyborgs.jpg",
     link: "https://www.google.com/books/edition/Natural-born_Cyborgs/6nw8DwAAQBAJ",
   },
   {
@@ -108,179 +95,156 @@ export const bookData = [
     subtitle:
       "How a Circle of Renegade Anthropologists Reinvented Race, Sex, and Gender in the Twentieth Century",
     author: "Charles King",
-    cover:
-      "https://books.google.com/books?id=jODrDwAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/godsupperair.jpg",
     link: "https://www.google.com/books/edition/Gods_of_the_Upper_Air/jODrDwAAQBAJ",
   },
   {
     title: "What Technology Wants",
     author: "Kevin Kelly",
-    cover:
-      "https://books.google.com/books?id=xbk0DwAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/whattechwants.jpg",
     link: "https://www.google.com/books/edition/What_Technology_Wants/xbk0DwAAQBAJ",
   },
   {
     title: "The Mushroom at the End of the World",
     subtitle: "On the Possibility of Life in Capitalist Ruins",
     author: "Anna Lowenhaupt Tsing",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0691178321.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/mushroom.jpg",
     link: "https://www.google.com/books/edition/The_Mushroom_at_the_End_of_the_World/MR4OEAAAQBAJ",
   },
   {
     title: "Coding Freedom",
     subtitle: "The Ethics and Aesthetics of Hacking",
     author: "E. Gabriella Coleman",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0691144613.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/codingfreedom.jpg",
     link: "https://www.google.com/books/edition/Coding_Freedom/O42iNERgx7oC",
   },
   {
     title: "Simians, Cyborgs, and Women",
     subtitle: "The Reinvention of Nature",
     author: "Donna Haraway",
-    cover:
-      "https://books.google.com/books?id=GG-R382kxIwC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/cyborg.jpg",
     link: "https://www.google.com/books/edition/Simians,_Cyborgs,_and_Women/GG-R382kxIwC",
   },
   {
     title: "Divining a Digital Future",
     subtitle: "Mess and Mythology in Ubiquitous Computing",
     author: "Paul Dourish",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0262015552.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/diviningdigital.jpg",
     link: "https://www.google.com/books/edition/Divining_a_Digital_Future/sT9AnwEACAAJ",
   },
   {
     title: "Sweetness and Power",
     subtitle: "The Place of Sugar in Modern History",
     author: "Sidney W. Mintz",
-    cover:
-      "https://books.google.com/books?id=k0xUDgAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/sweetnesspower.jpg",
     link: "https://www.google.com/books/edition/Sweetness_and_Power/k0xUDgAAQBAJ",
   },
   {
     title: "Working in Public",
     subtitle: "The Making and Maintenance of Open Source Software",
     author: "Nadia Eghbal",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0578675862.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/workinginpublic.jpg",
     link: "https://www.google.com/books/edition/Working_in_Public/EKeUzQEACAAJ",
   },
   {
     title: "Anthropology",
     subtitle: "Why It Matters",
     author: "Tim Ingold",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/1509519807.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/anthro.jpg",
     link: "https://www.google.com/books/edition/Anthropology/8GqHtAEACAAJ",
   },
   {
     title: "Debt, Updated and Expanded",
     subtitle: "The First 5,000 Years",
     author: "David Graeber",
-    cover:
-      "https://books.google.com/books?id=lliTBQAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/debt.jpg",
     link: "https://www.google.com/books/edition/Debt,_Updated_and_Expanded/lliTBQAAQBAJ",
   },
   {
     title: "Visual Explanations",
     subtitle: "Images and Quantities, Evidence and Narrative",
     author: "Edward R. Tufte",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0961392126.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/visualexp.jpg",
     link: "https://www.google.com/books/edition/Visual_Explanations/d-NOAAAAMAAJ",
   },
   {
     title: "Imagined Communities",
     subtitle: "Reflections on the Origin and Spread of Nationalism",
     author: "Benedict Anderson",
-    cover:
-      "https://books.google.com/books?id=nQ9jXXJV-vgC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/imagined.jpg",
     link: "https://www.google.com/books/edition/Imagined_Communities/nQ9jXXJV-vgC",
   },
   {
     title: "Finite and Infinite Games",
     author: "James Carse",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/I/51VCJR+H3QL._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/finitegames.jpg",
     link: "https://www.google.com/books/edition/Finite_and_Infinite_Games/ObLBJ_w2ZlcC",
   },
   {
     title: "Amusing Ourselves to Death",
     subtitle: "Public Discourse in the Age of Show Business",
     author: "Neil Postman",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/014303653X.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/amusing.jpg",
     link: "https://www.google.com/books/edition/Amusing_Ourselves_to_Death/oup6iagfox8C",
   },
   {
     title: "Making",
     subtitle: "Anthropology, Archaeology, Art and Architecture",
     author: "Tim Ingold",
-    cover:
-      "https://books.google.com/books?id=wwzUj2o42fUC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/making.jpg",
     link: "https://www.google.com/books/edition/Making/wwzUj2o42fUC",
   },
   {
     title: "Women, Fire, and Dangerous Things",
     author: "George Lakoff",
-    cover:
-      "https://books.google.com/books?id=CfCnzSF9de8C&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/womenfire.jpeg",
     link: "https://www.google.com/books/edition/Women,_Fire,_and_Dangerous_Things/CfCnzSF9de8C",
   },
   {
     title: "Purity and Danger",
     subtitle: "An Analysis of Concept of Pollution and Taboo",
     author: "Mary Douglas",
-    cover:
-      "https://books.google.com/books?id=J-CQRQwODVsC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/purity.jpg",
     link: "https://www.google.com/books/edition/Purity_and_Danger/J-CQRQwODVsC",
   },
   {
     title: "Addiction by Design",
     subtitle: "Machine Gambling in Las Vegas",
     author: "Natasha Dow Schüll",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/0691127557.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/addiction.jpg",
     link: "https://www.google.com/books/edition/Addiction_by_Design/XmqYDwAAQBAJ",
   },
   {
     title: "Descartes' Error",
     subtitle: "Emotion, Reason and the Human Brain",
     author: "Antonio Damasio",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/P/014303622X.01._SX900_SY1270_SCLZZZZZZZ_.jpg",
+    cover: "/images/books/descarteserror.jpg",
     link: "https://www.google.com/books/edition/Descartes'_Error/MRY3hmYc1W8C",
   },
   {
     title: "Meat Planet",
     subtitle: "Artificial Flesh and the Future of Food",
     author: "Benjamin Wurgaft",
-    cover:
-      "https://pics.cdn.librarything.com/picsizes/d5/d7/d5d76b9517ffcfe596c4b427751426f41514141_v5.jpg",
+    cover: "/images/books/meat.jpg",
     link: "https://www.google.com/books/edition/Meat_Planet/znI-EAAAQBAJ",
   },
   {
     title: "The Gift",
     subtitle: "The Form and Reason for Exchange in Archaic Societies",
     author: "Marcel Mauss",
-    cover:
-      "https://books.google.com/books?id=zxsiBQAAQBAJ&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/gift.jpg",
     link: "https://www.google.com/books/edition/The_Gift/zxsiBQAAQBAJ",
   },
   {
     title: "We Have Never Been Modern",
     author: "Bruno Latour",
-    cover:
-      "https://books.google.com/books?id=xbnK8NzMsm4C&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/modern.jpg",
     link: "https://www.google.com/books/edition/We_Have_Never_Been_Modern/xbnK8NzMsm4C",
   },
   {
     title: "Metaphors We Live By",
     author: "George Lakoff",
-    cover:
-      "https://books.google.com/books?id=r6nOYYtxzUoC&printsec=frontcover&img=1&zoom=3&source=gbs_api",
+    cover: "/images/books/mwlb.jpg",
     link: "https://www.google.com/books/edition/Metaphors_We_Live_By/r6nOYYtxzUoC",
   },
 ];
