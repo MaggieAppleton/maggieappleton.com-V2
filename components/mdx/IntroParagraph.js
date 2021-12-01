26e9dda6e7dffc763787b61dadd295c40f8c397f
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2021-12-01 21:48:38 +0000

    added more assumed audiences

--- components/mdx/IntroParagraph.js
+++ components/mdx/IntroParagraph.js
@@ -2,25 +2,25 @@ import styled from "styled-components";
 import { breakpoints } from "../../utils/breakpoints";
 
 const IntroParagraph = styled.p`
+  :first-letter {
+    float: left;
+    font-size: 5.2rem;
+    line-height: 2.5rem;
+    font-weight: bold;
+    margin: 1.15rem var(--space-2xs) 0 0;
+  }
+  p {
+    margin-bottom: 0;
+  }
+  @media ${breakpoints.mediaSM} {
     :first-letter {
-        float: left;
-        font-size: 5.4rem;
-        line-height: 3rem;
-        font-weight: bold;
-        margin: 1.15rem var(--space-2xs) 0 0;
+      float: left;
+      font-size: 4.6rem;
+      line-height: 2rem;
+      font-weight: bold;
+      margin: 1.15rem var(--space-3xs) 0 0;
     }
-    p {
-        margin-bottom: 0;
-    }
-    @media ${breakpoints.mediaSM} {
-        :first-letter {
-            float: left;
-            font-size: 4.6rem;
-            line-height: 2rem;
-            font-weight: bold;
-            margin: 1.15rem var(--space-3xs) 0 0;
-        }
-    }
+  }
 `;
 
 export default IntroParagraph;
