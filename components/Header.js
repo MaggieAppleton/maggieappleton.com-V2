a16bb6f72eaaddad6455faa97a6b58841c9f7776
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2021-12-02 09:25:23 +0000

    added next-seo headers

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,42 @@
 import Head from "next/head";
+import { NextSeo } from "next-seo";
+
+//https://github.com/garmeeh/next-seo
 
 export default function Header({ title, description, keywords }) {
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta
-                name="description"
-                content={
-                    description
-                        ? description
-                        : "Maggie's digital garden filled with visual essays on programming, design, and anthropology"
-                }
-            />
-            <meta
-                name="keywords"
-                content={
-                    keywords
-                        ? keywords
-                        : "design, programming, anthropology, illustration, digital garden, metaphors"
-                }
-            />
-            <meta name="author" content="Maggie Appleton" />
-        </Head>
-    );
+  return (
+    <NextSeo
+      title={title}
+      description={description}
+      openGraph={{
+        url: "https://maggieappleton.com",
+        title: title,
+        description: description,
+
+        // images: [
+        //   {
+        //     url: "https://www.example.ie/og-image-01.jpg",
+        //     width: 800,
+        //     height: 600,
+        //     alt: "Og Image Alt",
+        //     type: "image/jpeg",
+        //   },
+        //   {
+        //     url: "https://www.example.ie/og-image-02.jpg",
+        //     width: 900,
+        //     height: 800,
+        //     alt: "Og Image Alt Second",
+        //     type: "image/jpeg",
+        //   },
+        //   { url: "https://www.example.ie/og-image-03.jpg" },
+        //   { url: "https://www.example.ie/og-image-04.jpg" },
+        // ],
+        site_name: "Maggie Appleton",
+      }}
+      twitter={{
+        handle: "@mappletons",
+        cardType: "summary_large_image",
+      }}
+    />
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,41 @@
 import { GlobalStyle } from "../components/GlobalStyle";
 // import { AnimatePresence } from "framer-motion";
 import "./_app.css";
-// import { DefaultSeo } from "next-seo";
+import { DefaultSeo } from "next-seo";
 import Navbar from "../components/Navbar/index.js";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps, router }) {
-    return (
-        <>
-            <GlobalStyle />
-            <container>
-                {/* <DefaultSeo
-                    openGraph={{
-                        type: "website",
-                        locale: "en_GB",
-                        url: "https://www.maggieappleton.com/",
-                        site_name: "Maggie Appleton",
-                    }}
-                    twitter={{
-                        handle: "@mappletons",
-                        cardType: "summary_large_image",
-                    }}
-                /> */}
-                <Navbar />
-                {/* <AnimatePresence
+  return (
+    <>
+      <GlobalStyle />
+      <container>
+        <DefaultSeo
+          title="Maggie Appleton's digital garden"
+          description="Maggie's digital garden filled with visual essays on programming, design, and anthropology"
+          openGraph={{
+            type: "website",
+            locale: "en_GB",
+            url: "https://www.maggieappleton.com/",
+            site_name: "Maggie Appleton",
+          }}
+          twitter={{
+            handle: "@mappletons",
+            cardType: "summary_large_image",
+          }}
+        />
+        <Navbar />
+        {/* <AnimatePresence
                     exitBeforeEnter
                     initial={false}
                     onExitComplete={() => window.scrollTo(0, 0)}
                 > */}
-                <Component {...pageProps} key={router.route} />
-                {/* </AnimatePresence> */}
-                <Footer />
-            </container>
-        </>
-    );
+        <Component {...pageProps} key={router.route} />
+        {/* </AnimatePresence> */}
+        <Footer />
+      </container>
+    </>
+  );
 }
 
 export default MyApp;
