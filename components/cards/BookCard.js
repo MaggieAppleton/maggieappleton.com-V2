13c032d68e8d4ec334e63e69bab33fe4441e280c
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2021-12-13 08:39:15 +0000

    add hover links on books

--- components/cards/BookCard.js
+++ components/cards/BookCard.js
@@ -1,68 +1,100 @@
 import styled from "styled-components";
+import { ExternalLinkIcon } from "@heroicons/react/solid";
 
 export default function BookCard({
-    small,
-    cover,
-    title,
-    subtitle,
-    author,
-    link,
+  small,
+  cover,
+  title,
+  subtitle,
+  author,
+  link,
 }) {
-    return (
-        <a
-            style={{ cursor: "pointer" }}
-            href={link}
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <StyledBookCard small={small}>
-                <img src={cover} alt={title} />
-                <div>
-                    <p>
-                        {title}
-                        {!small && subtitle && `: ${subtitle}`}
-                    </p>
-                    <span className="metadata">{author}</span>
-                </div>
-            </StyledBookCard>
-        </a>
-    );
+  return (
+    <a
+      style={{ cursor: "pointer" }}
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <StyledBookCard small={small}>
+        <ExternalHoverLink small={small}>
+          View on Google Books{" "}
+          <ExternalLinkIcon
+            width="18"
+            height="18"
+            style={{ position: "relative", top: "3px" }}
+          />
+        </ExternalHoverLink>
+        <img src={cover} alt={title} />
+        <div>
+          <p>
+            {title}
+            {!small && subtitle && `: ${subtitle}`}
+          </p>
+          <span className="metadata">{author}</span>
+        </div>
+      </StyledBookCard>
+    </a>
+  );
 }
 
+const ExternalHoverLink = styled.a`
+  position: absolute;
+  top: 35%;
+  left: ${(props) => (props.small ? "10%" : "18%")};
+  transform-origin: center;
+  z-index: 1;
+  font-family: var(--font-sans);
+  background: white;
+  color: var(--color-gray-800);
+  padding: var(--space-3xs) var(--space-2xs);
+  border-radius: var(--border-radius-base);
+  box-shadow: var(--box-shadow-md);
+  opacity: 0%;
+  transition: opacity 0.3s ease-in-out;
+`;
+
 const StyledBookCard = styled.div`
-    display: flex;
-    flex-direction: column;
-    transition: all 0.4s ease-in-out;
-    margin: 0 var(--space-2xs) var(--space-2xs) 0;
-    padding: ${(props) =>
-        props.small
-            ? "0 0 var(--space-xs) 0"
-            : "0 var(--space-3xs) var(--space-l) var(--space-3xs)"};
+  display: flex;
+  position: relative;
+  top: 0;
+  left: 0;
+  flex-direction: column;
+  transition: all 0.3s ease-in-out;
+  margin: 0 var(--space-2xs) var(--space-2xs) 0;
+  padding: ${(props) =>
+    props.small
+      ? "0 0 var(--space-xs) 0"
+      : "0 var(--space-3xs) var(--space-l) var(--space-3xs)"};
+  img {
+    transition: all 0.3s ease-in-out;
+    border-radius: var(--border-radius-base);
+    box-shadow: var(--box-shadow-sm);
+    border: 1px solid var(--color-tinted-cream);
+  }
+  div {
+    transition: all 0.3s ease-in-out;
+  }
+  div p {
+    transition: all 0.3s ease-in-out;
+    margin: var(--space-2xs) 0 var(--space-3xs) 0;
+    line-height: var(--leading-base);
+    color: var(--color-black);
+  }
+  &:hover {
     img {
-        transition: all 0.4s ease-in-out;
-        border-radius: var(--border-radius-base);
-        box-shadow: var(--box-shadow-sm);
-        border: 1px solid var(--color-tinted-cream);
+      box-shadow: var(--box-shadow-lg);
+      transform: scale3d(1.02, 1.02, 1.02);
+      opacity: 20%;
+    }
+    p {
+      color: var(--color-crimson);
     }
     div {
-        transition: all 0.4s ease-in-out;
+      transform: translateY(4px);
     }
-    div p {
-        transition: all 0.4s ease-in-out;
-        margin: var(--space-2xs) 0 var(--space-3xs) 0;
-        line-height: var(--leading-base);
-        color: var(--color-black);
+    ${ExternalHoverLink} {
+      opacity: 100%;
     }
-    &:hover {
-        img {
-            box-shadow: var(--box-shadow-lg);
-            transform: scale3d(1.02, 1.02, 1.02);
-        }
-        p {
-            color: var(--color-crimson);
-        }
-        div {
-            transform: translateY(4px);
-        }
-    }
+  }
 `;
