3a5ddffee52f58a2280c19356daf3494ac4644ca
Dave Bernhard <davidbernhard@live.com>
2022-01-09 15:21:09 +0000

    make ready for prod

--- utils/getOgImage.js
+++ utils/getOgImage.js
@@ -3,19 +3,17 @@ const { createHash } = require('crypto');
 const fs = require('fs');
 
 async function getOgImage(path) {
+  if (process.env.NODE_ENV === 'development') {
+    return 'og image will be generated in production';
+  }
 
-  // TODO: uncomment for production
-  // if (process.env.NODE_ENV === 'development') {
-  //   return 'og image will be generated in production';
-  // }
-
-  const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://maggieappleton.com';
+  const baseUrl = 'https://maggieappleton.com';
   const url = `${baseUrl}${path}`;
   const hash = createHash('md5').update(url).digest('hex');
   const browser = await playwright.launchChromium({ headless: true });
   const ogImageDir = `./public/images/og`;
   const imagePath = `${ogImageDir}/${hash}.png`;
-  const publicPath = `${process.env.BASE_URL ?? 'http://localhost:3000'}/images/og/${hash}.png`;
+  const publicPath = `${process.env.BASE_URL}/images/og/${hash}.png`;
 
   try {
     fs.statSync(imagePath);
@@ -26,8 +24,7 @@ async function getOgImage(path) {
 
   const page = await browser.newPage();
   await page.setViewportSize({ width: 1200, height: 630 });
-  // TODO: uncomment for production
-  // await page.goto(url, { waitUntil: 'networkidle' });
+  await page.goto(url, { waitUntil: 'networkidle' });
   await page.goto(url);
   const buffer = await page.screenshot({ type: 'png' });
   await browser.close();
