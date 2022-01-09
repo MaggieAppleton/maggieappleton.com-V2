297c9d791bb138400fcf7977203169f43e909acc
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-01-09 21:43:08 +0000

    styling yo

--- pages/og-image.js
+++ pages/og-image.js
@@ -1,8 +1,12 @@
 import { useRouter } from "next/router";
 import GrowthIcon from "../components/icons/GrowthIcon";
 import Logo from "../components/visuals/Logo";
+import styled from "styled-components";
 
 // Example URL: http://localhost:3000/og-image?title=A%20Brief%20History%20of%20the%20Digital%20Garden&subtitle=A%20newly%20revived%20philosophy%20for%20publishing%20personal%20knowledge%20on%20the%20web&postType=essay&growthStage=evergreen&cover=/images/covers/garden-cover@2x.png
+
+// Example URL: http://localhost:3000/og-image?title=A%20Brief%20History%20of%20the%20Digital%20Garden&subtitle=A%20newly%20revived%20philosophy%20for%20publishing%20personal%20knowledge%20on%20the%20web&postType=essay&growthStage=evergreen
+
 const OgImage = () => {
   const router = useRouter();
   const searchParams = new URLSearchParams(router.asPath.split(/\?/)[1]);
@@ -14,39 +18,85 @@ const OgImage = () => {
   const cover = searchParams.get("cover");
 
   return (
-    <div style={{ width: 1200, height: 630, padding: '46px 64px'}}>
+    <div
+      style={{
+        width: 1200,
+        height: 630,
+        padding: "var(--space-xl)",
+        border: "1px solid red",
+      }}
+    >
       {postType && growthStage ? (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          {postType} <GrowthIcon growthStage={growthStage} /> {growthStage}
+        <div style={{ marginBottom: "var(--space-xs)" }}>
+          <StyledType>{postType}</StyledType>{" "}
+          <GrowthIcon growthStage={growthStage} />{" "}
+          <StyledGrowthStage>{growthStage}</StyledGrowthStage>
         </div>
       ) : null}
-      <div style={{ display: 'flex' }}>
-        <div style={{ width: cover ? '50%' : '100%' }}>
-          <h1
-            style={{
-              fontSize: 'var(--font-size-2xl)',
-              lineHeight: 'var(--leading-tighter)',
-              wordWrap: 'break-word'
-            }}>{title}</h1>
-          {subtitle &&
-            <h2
-              style={{
-                fontFamily: 'var(--font-body)',
-                fontWeight: '300',
-                fontSize: 'var(--font-size-lg)',
-                lineHeight: 'var(--leading-tighter)',
-                color: 'var(--color-gray-800)',
-                wordWrap: 'break-word'
-              }}>{subtitle}</h2>}
+      <div style={{ marginBottom: "var(--space-l)" }}>
+        <div style={{ width: cover ? "60%" : "100%" }}>
+          <StyledTitle>{title}</StyledTitle>
+          {subtitle && <StyledSubtitle>{subtitle}</StyledSubtitle>}
         </div>
-        {cover ? <div style={{ width: '475px' }}><img src={cover} /></div> : null}
+        {cover ? (
+          <div style={{ width: "475px" }}>
+            <img src={cover} />
+          </div>
+        ) : null}
       </div>
-      <div>
-        <span>maggieappleton.com</span>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <StyledSiteURL>maggieappleton.com</StyledSiteURL>
         <Logo />
       </div>
     </div>
   );
 };
+
+const StyledTitle = styled.h1`
+  font-family: var(--font-serif);
+  font-size: var(--font-size-3xl);
+  line-height: var(--leading-tighter);
+  max-width: 80%;
+`;
+
+const StyledSubtitle = styled.h2`
+  font-family: var(--font-serif);
+  font-size: var(--font-size-lg);
+  margin: var(--space-s) 0 0 0;
+  color: var(--color-gray-800);
+  font-weight: 100;
+  line-height: var(--leading-tight);
+  max-width: 80%;
+`;
+
+const StyledSiteURL = styled.p`
+  font-family: var(--font-body);
+  font-weight: 300;
+  font-size: var(--font-size-md);
+  color: var(--color-crimson);
+`;
+
+const StyledGrowthStage = styled.p`
+  display: inline-block;
+  cursor: pointer;
+  font-family: var(--font-sans);
+  font-size: var(--font-size-xs);
+  color: var(--color-gray-800);
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  font-weight: bold;
+  padding-right: var(--space-xs);
+`;
+
+const StyledType = styled.span`
+  display: inline-block;
+  font-family: var(--font-sans);
+  font-size: var(--font-size-xs);
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
+  font-weight: bold;
+  padding-right: var(--space-xs);
+  color: var(--color-bright-crimson);
+`;
 
 export default OgImage;
