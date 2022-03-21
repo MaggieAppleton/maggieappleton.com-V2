742348912846c3dd5aacf53030104e1ab46e13f3
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-03-21 09:17:12 +0000

    updated simeplcard props

--- components/mdx/SimpleCard.js
+++ components/mdx/SimpleCard.js
@@ -1,30 +1,40 @@
 import styled from "styled-components";
 
-export default function SimpleCard({ children, alignLeft }) {
-    return <StyledCard alignLeft={alignLeft}>{children}</StyledCard>;
+export default function SimpleCard({ children, alignLeft, subtle, padding }) {
+  return (
+    <StyledCard padding={padding} subtle={subtle} alignLeft={alignLeft}>
+      {children}
+    </StyledCard>
+  );
 }
 
 const StyledCard = styled.div`
-    margin: var(--space-3xs) auto var(--space-m);
-    box-shadow: var(--box-shadow-lg);
-    border-radius: var(--border-radius-lg);
-    border: 1px solid var(--color-tinted-cream);
-    display: flex;
-    flex-direction: column;
-    align-items: ${(props) => (props.alignLeft ? "" : "center")};
-    justify-content: center;
-    text-align: ${(props) => (props.alignLeft ? "" : "center")};
-    padding: var(--space-m) var(--space-l);
-    grid-column: 1/4 !important;
-    max-width: 860px;
-    width: 100%;
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    p,
-    .tweet {
-        margin: var(--space-2xs) 0;
-    }
+  margin: ${(props) =>
+    props.subtle
+      ? "0 auto var(--space-s)"
+      : "var(--space-3xs) auto var(--space-m)"};
+  box-shadow: ${(props) =>
+    props.subtle ? "var(--box-shadow-sm)" : "var(--box-shadow-lg)"};
+  border-radius: var(--border-radius-lg);
+  border: 1px solid var(--color-tinted-cream);
+  display: flex;
+  flex-direction: column;
+  align-items: ${(props) => (props.alignLeft ? "" : "center")};
+  justify-content: center;
+  text-align: ${(props) => (props.alignLeft ? "" : "center")};
+  padding: ${(props) =>
+    props.padding ? props.padding : "var(--space-m) var(--space-l)"};
+  grid-column: 1/4 !important;
+  max-width: 860px;
+  width: 100%;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  p,
+  ol,
+  ul .tweet {
+    margin: var(--space-2xs) 0;
+  }
 `;
