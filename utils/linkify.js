fad696a26782a7f824759839b4124581baa24407
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-07-10 11:07:46 -0400

    refactoring linkify

--- utils/linkify.js
+++ utils/linkify.js
@@ -1,5 +1,5 @@
-import linkMaps from '../links.json'
-import { getBracketPairs } from './bracketPairs'
+import linkMaps from "../links.json";
+import { getBracketPairs } from "./bracketPairs";
 
 /**
  * This replaces double bracketed links [[like this one]] in the markdown for
@@ -10,48 +10,57 @@ import { getBracketPairs } from './bracketPairs'
  * @returns transformed markdown string for blog post
  */
 export function linkify(content, title) {
-    if (!content) return content
+  if (!content) return content;
 
-    const matchingBracketPairs = getBracketPairs(content)
-    if (matchingBracketPairs.length < 1) return content
+  const matchingBracketPairs = getBracketPairs(content);
+  if (matchingBracketPairs.length < 1) return content;
 
-    // Get all outbound links for this post
-    const outboundLinks = linkMaps.find((map) => map.ids[0] === title)?.outboundLinks
+  // Get all outbound links for this post
+  const outboundLinks = linkMaps.find(
+    (map) => map.ids[0] === title
+  )?.outboundLinks;
 
-    let result = ''
-    let previousIndex = 0 // Starts from first character of the content string
+  let result = "";
+  let previousIndex = 0; // Starts from first character of the content string
 
-    // For each pair of brackets (link) found, append the content found before to the result,
-    // then append the link itself with JSX replacing the brackets, then append the rest of
-    // the markdown string until the next pair of brackets
-    matchingBracketPairs.forEach((pair) => {
-        const opening = pair[0]
-        const closing = pair[1]
-        const foundLinkText = content.substring(opening + 1, closing - 1)
-        const matchedOutboundLink =
-            outboundLinks &&
-            outboundLinks.find((ol) => ol.matchedId.toLowerCase() === foundLinkText.toLowerCase())
+  // For each pair of brackets (link) found, append the content found before to the result,
+  // then append the link itself with JSX replacing the brackets, then append the rest of
+  // the markdown string until the next pair of brackets
+  matchingBracketPairs.forEach((pair) => {
+    const opening = pair[0];
+    const closing = pair[1];
+    const foundLinkText = content.substring(opening + 1, closing - 1);
+    const matchedOutboundLink =
+      outboundLinks &&
+      outboundLinks.find(
+        (ol) => ol.matchedId.toLowerCase() === foundLinkText.toLowerCase()
+      );
 
-        if (matchedOutboundLink) {
-            // todo: destructure title, excerpt and growthStage for your tooltip
-            const { slug } = matchedOutboundLink;
+    if (matchedOutboundLink) {
+      // todo: destructure title, excerpt and growthStage for your tooltip
+      const {
+        slug,
+        title: matchedTitle,
+        description: matchedDescription,
+        excerpt,
+      } = matchedOutboundLink;
 
-            // todo: replace ordinary anchor tag for customised tooltip and internal next Link combo
-            result += content.substring(previousIndex, opening - 1) // append content up to link
-            result += `<a href={'/${slug}'}>` // append JSX opening tags
-            result += foundLinkText // skip opening brackets, then append link content (referenced post title or alias)
-            result += '</a>' // append JSX closing tags
-        } else {
-            result += content.substring(previousIndex, closing)
-        }
-        previousIndex = closing + 1 // skip closing brackets and start new loop until reaching end of the bracket pairs
-    })
+      // todo: replace ordinary anchor tag for customised tooltip and internal next Link combo
+      result += content.substring(previousIndex, opening - 1); // append content up to link
+      result += `<InternalTooltipLink title={"${matchedTitle}"} description={"${matchedDescription}"}  href={"/${slug}"} excerpt={"${excerpt}"}>`; // append JSX opening tags
+      result += foundLinkText; // skip opening brackets, then append link content (referenced post title or alias)
+      result += "</InternalTooltipLink>"; // append JSX closing tags
+    } else {
+      result += content.substring(previousIndex, closing);
+    }
+    previousIndex = closing + 1; // skip closing brackets and start new loop until reaching end of the bracket pairs
+  });
 
-    const numPairs = matchingBracketPairs.length
-    const lastClosingBracket = matchingBracketPairs[numPairs - 1][1]
+  const numPairs = matchingBracketPairs.length;
+  const lastClosingBracket = matchingBracketPairs[numPairs - 1][1];
 
-    // Append content from last bracket pair to end of content
-    result += content.substring(lastClosingBracket + 1, content.length - 1)
+  // Append content from last bracket pair to end of content
+  result += content.substring(lastClosingBracket + 1, content.length - 1);
 
-    return result
+  return result;
 }
