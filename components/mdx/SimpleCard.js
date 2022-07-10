a92fe48909fea9efafb261cdd714ef0e3a1922b9
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-07-10 15:10:05 -0400

    refactored subtle cards

--- components/mdx/SimpleCard.js
+++ components/mdx/SimpleCard.js
@@ -1,21 +1,28 @@
 import styled from "styled-components";
 
-export default function SimpleCard({ children, alignLeft, subtle, padding }) {
-  return (
-    <StyledCard padding={padding} subtle={subtle} alignLeft={alignLeft}>
-      {children}
-    </StyledCard>
-  );
+export default function SimpleCard({
+  children,
+  alignLeft,
+  subtle,
+  padding,
+  margin,
+}) {
+  if (subtle) {
+    return <SubtleCard>{children}</SubtleCard>;
+  } else {
+    return (
+      <StyledCard padding={padding} margin={margin} alignLeft={alignLeft}>
+        {children}
+      </StyledCard>
+    );
+  }
 }
 
 const StyledCard = styled.div`
   margin: ${(props) =>
-    props.subtle
-      ? "0 auto var(--space-s)"
-      : "var(--space-3xs) auto var(--space-m)"};
-  box-shadow: ${(props) =>
-    props.subtle ? "var(--box-shadow-sm)" : "var(--box-shadow-lg)"};
-  border-radius: var(--border-radius-lg);
+    props.margin ? props.margin : "var(--space-3xs) auto var(--space-m)"};
+  box-shadow: var(--box-shadow-lg);
+  border-radius: var(--border-radius-base);
   border: 1px solid var(--color-tinted-cream);
   display: flex;
   flex-direction: column;
@@ -25,7 +32,7 @@ const StyledCard = styled.div`
   padding: ${(props) =>
     props.padding ? props.padding : "var(--space-m) var(--space-l)"};
   grid-column: 1/4 !important;
-  max-width: 860px;
+  max-width: 848px;
   width: 100%;
   h1,
   h2,
@@ -37,4 +44,9 @@ const StyledCard = styled.div`
   ul .tweet {
     margin: var(--space-2xs) 0;
   }
+`;
+
+const SubtleCard = styled(StyledCard)`
+  margin: 0 auto var(--space-s);
+  box-shadow: var(--box-shadow-sm);
 `;
