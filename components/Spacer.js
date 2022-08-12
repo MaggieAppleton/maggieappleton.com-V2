a5a8bb6f2550e779690e2d4fddd269d15d296754
Maggie Appleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-08-12 11:52:50 +0100

    added xs size to spacer

--- components/Spacer.js
+++ components/Spacer.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 const handleSize = (size) => {
   switch (size) {
+    case "xs":
+      return "var(--space-m)";
     case "small":
       return "var(--space-l)";
     case "large":
