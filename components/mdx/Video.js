c1f2c61e7ee5cbfc533f4b0a5dd7d3abe403fe18
MaggieAppleton <5599295+MaggieAppleton@users.noreply.github.com>
2022-11-14 15:49:27 +0000

    wrote command bars, updated video component

--- components/mdx/Video.js
+++ components/mdx/Video.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const Video = ({ src, title, width, height, margin, ...props }) => (
   <StyledVideo margin={margin} {...props}>
     <iframe
-      width={ width ? width : "853"}
-      height={ height ? height : "480"}
+      width={ width ? width : "1000"}
+      height={ height ? height : "570"}
       src={src}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -15,16 +15,26 @@ const Video = ({ src, title, width, height, margin, ...props }) => (
 );
 
 const StyledVideo = styled.div`
-  overflow: hidden;
-  padding-bottom: 56.25%;
+  margin: ${(props) => {props.margin || "2rem auto"}};
+  grid-column: 1/4 !important;
+  width: 950px;
+  height: 570px;
   position: relative;
-  height: 0;
-  margin: ${(props) => {props.margin || null}};
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: var(--box-shadow-lg);
+  @media (max-width: 724px) {
+      width: 100%;
+      height: 50vw;
+  }
   iframe {
     left: 0;
     top: 0;
     height: 100%;
-    width: 100%;
+    max-width: 100%;
     position: absolute;
     @media (max-width: 724px) {
       max-width: 100%;
