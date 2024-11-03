6144b8c3968ac11fa86a0fed348c5090e51a24e7
MaggieAppleton <5599295+MaggieAppleton@users.noreply.github.com>
2024-11-03 12:49:39 +0000

    fixed line height

--- components/mdx/YearsAgo.js
+++ components/mdx/YearsAgo.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
-const StyledSup = styled.sub`
+const Container = styled.span`
+	display: inline-block;
+	line-height: 1.5;
+`;
+
+const StyledSub = styled.sub`
 	font-family: var(--font-sans);
 	font-weight: 600;
 	font-size: var(--font-size-xs);
@@ -14,8 +19,7 @@ export default function YearsAgo({ children }) {
 	const yearMatch = children.toString().match(/\d{4}/);
 	const year = yearMatch ? parseInt(yearMatch[0]) : null;
 
-	if (!year) return children; // Return original content if no valid year found
-
+	if (!year) return children;
 	const currentYear = new Date().getFullYear();
 	const yearsAgo = currentYear - year;
 
@@ -23,17 +27,17 @@ export default function YearsAgo({ children }) {
 	const hasParens = children.toString().startsWith("(");
 
 	return (
-		<span title={`${year} was ${yearsAgo} years ago`}>
+		<Container title={`${year} was ${yearsAgo} years ago`}>
 			{hasParens ? (
 				<>
-					({year})<StyledSup>{yearsAgo}ya</StyledSup>
+					({year})<StyledSub>{yearsAgo}ya</StyledSub>
 				</>
 			) : (
 				<>
 					{year}
-					<StyledSup>{yearsAgo}ya</StyledSup>
+					<StyledSub>{yearsAgo}ya</StyledSub>
 				</>
 			)}
-		</span>
+		</Container>
 	);
 }
